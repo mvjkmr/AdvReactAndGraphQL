2ac28347791fb7f40ba5f12ab698d145b7df739b
Vijay Kumar M <vijaykumar.mylapalli@outlook.com>
2021-02-24 23:47:54 +0530

    styled components- nav

--- sick-fits/frontend/components/Header.js
+++ sick-fits/frontend/components/Header.js
@@ -1,12 +1,51 @@
+import Link from 'next/link';
+import styled from 'styled-components';
 import Nav from './Nav';
+
+const Logo = styled.h1`
+  background-color: red;
+  font-size: 4rem;
+  margin-left: 2rem;
+  position: relative;
+  z-index: 2;
+  transform: skew(-7deg);
+
+  a {
+    color: white;
+    text-decoration: none;
+    text-transform: uppercase;
+    padding: 0.5rem 1rem;
+  }
+`;
+
+const HeaderStyles = styled.header`
+  .bar {
+    border-bottom: 10px solid var(--black, black);
+    display: grid;
+    grid-template-columns: auto 1fr;
+    justify-content: space-between;
+    align-items: center;
+  }
+
+  .sub-bar {
+    border-bottom: 1px solid var(--black, black);
+    display: grid;
+    grid-template-columns: 1fr auto;
+  }
+`;
 
 export default function Header() {
   return (
-    <div>
-      <header>I am the header</header>
+    <HeaderStyles>
+      <div className="bar">
+        <Logo>
+          <Link href="/">sick-fits</Link>
+        </Logo>
+      </div>
+      <div className="sub-bar">
+        <p>Search</p>
+      </div>
       <Nav />
-      <div className="bar">sick-fits</div>
-      <div className="sub-bar">search</div>
-    </div>
+    </HeaderStyles>
   );
 }
